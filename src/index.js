b59c9fc71b78819a1d3a6d08935f0582162de78f
CaffeineFusion <owen.smith@servian.com>
2017-07-24 11:43:11 +1000

    removing build files to reduce complexity for demo

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ChatBot from './components/ChatBot.jsx';
-
-//console.log(google);
-ReactDOM.render(
-    <ChatBot maps={window.google} />,
-    document.getElementById('chat-bot')
-);
